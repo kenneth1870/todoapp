da3be73289db9d540f643eb64e561ef9f0259594
Luis Antonio Sibaja Cantillano <lsibaja@asurint.com>
2019-05-08 14:59:35 -0600

    Auth and jwt

--- server/src/routes/todoRoutes.js
+++ server/src/routes/todoRoutes.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const todoRouter = express.Router();
+const passport = require('passport');
 const todoController = require('../controllers/todoController');
 let Todo = require('../models/todo.model');
-
 const { getIndex, getById, add, update } = todoController(Todo);
+const todoRouter = express.Router();
 
 todoRouter.route('/')
-  .get(getIndex)
-  .post(add);
+  .get(passport.authenticate('jwt', { session: false }), getIndex)
+  .post(passport.authenticate('jwt', { session: false }), add);
 
 todoRouter.route('/:id')
-  .get(getById)
-  .put(update);
+  .get(passport.authenticate('jwt', { session: false }), getById)
+  .put(passport.authenticate('jwt', { session: false }), update);
 
 module.exports = todoRouter;
